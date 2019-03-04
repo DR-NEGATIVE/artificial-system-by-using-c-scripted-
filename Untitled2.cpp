55415505364766c1495a26d18d61e0a56a2231c4
ojhastar01 <45567648+ojhastar01@users.noreply.github.com>
2019-03-04 21:01:08 +0530

    c++ ai scripted code
    Hey friends try this code in c++ compiler  or dev c++ fully tested on devC++

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,533 +1,159 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <conio.h>
+#include<stdio.h>
+#include<string.h>
 #include<time.h>
-#include<ctype.h>
-#include <time.h>
+#include<stdlib.h>
+#include<dos.h>
+#include <bits/stdc++.h>
 #include <windows.h>
-#include <process.h>
+int main(){
+	char s[1055];
+    //try this code in dev c++ or c++ compiler some funcation are written in c++
+printf("Hi i am ACCO,I am a scripted Artificial inteligence System \nReady For Your command\nfor calculation Use add,multiply,divide as command :-)\n");
+printf("\t  *********\n\t  ***********\n\t  *************\n\t******************\n\t******************\n\t  *************\n\t  ***********\n\t  *********\n");
+message:gets(s);
 
-#define UP 100
-#define DOWN 100
-#define LEFT 100
-#define RIGHT 100
-
-int length;
-int bend_no;
-int len;
-char key;
-void record();
-void load();
-int life;
-void Delay(long double);
-void Move();
-void Food();
-int Score();
-void Print();
-void gotoxy(int x, int y);
-void GotoXY(int x,int y);
-void Bend();
-void Boarder();
-void Down();
-void Left();
-void Up();
-void Right();
-void ExitGame();
-int Scoreonly();
-
-struct coordinate{
-    int x;
-    int y;
-    int direction;
-};
-
-typedef struct coordinate coordinate;
-
-coordinate head, bend[500],food,body[30];
-
-int main()
+if(strcasecmp ("hello",s)==0||strcasecmp("hey",s)==0||strcasecmp("hi acco",s)==0)
 {
 
-    char key;
-
-    Print();
-
-    system("cls");
-
-    load();
-
-    length=5;
-
-    head.x=25;
-
-    head.y=20;
-
-    head.direction=RIGHT;
-
-    Boarder();
-
-    Food(); //to generate food coordinates initially
-
-    life=3; //number of extra lives
-
-    bend[0]=head;
-
-    Move();   //initialing initial bend coordinate
-
-    return 0;
-
+printf("Hello sir,How can I Help You :-)\n");
+goto message;
 }
-
-void Move()
+else if(strcasecmp("acco",s)==0)
 {
-    int a,i;
-
-    do{
-
-        Food();
-        fflush(stdin);
-
-        len=0;
-
-        for(i=0;i<30;i++)
-
-        {
-
-            body[i].x=0;
-
-            body[i].y=0;
-
-            if(i==length)
-
-            break;
-
-        }
-
-        Delay(length);
-
-        Boarder();
-
-        if(head.direction==RIGHT)
-
-            Right();
-
-        else if(head.direction==LEFT)
-
-            Left();
-
-        else if(head.direction==DOWN)
-
-            Down();
-
-        else if(head.direction==UP)
-
-            Up();
-
-        ExitGame();
-
-    }while(!kbhit());
-
-    a=getch();
-
-    if(a==27)
-
-    {
-
-        system("cls");
-
-        exit(0);
-
-    }
-    key=getch();
-
-    if((key==RIGHT&&head.direction!=LEFT&&head.direction!=RIGHT)||(key==LEFT&&head.direction!=RIGHT&&head.direction!=LEFT)||(key==UP&&head.direction!=DOWN&&head.direction!=UP)||(key==DOWN&&head.direction!=UP&&head.direction!=DOWN))
-
-    {
-
-        bend_no++;
-
-        bend[bend_no]=head;
-
-        head.direction=key;
-
-        if(key==UP)
-
-            head.y--;
-
-        if(key==DOWN)
-
-            head.y++;
-
-        if(key==RIGHT)
-
-            head.x++;
-
-        if(key==LEFT)
-
-            head.x--;
-
-        Move();
-
-    }
-
-    else if(key==27)
-
-    {
-
-        system("cls");
-
-        exit(0);
-
-    }
-
-    else
-
-    {
-
-        printf("\a");
-
-        Move();
-
-    }
+	printf("Yes sir any Help\n");
+	goto message;
 }
-
-void gotoxy(int x, int y)
+else if(strcasecmp("Who makes you",s)==0||strcasecmp("who design you",s)==0||strcasecmp("who is acco",s)==0)
 {
 
- COORD coord;
+printf("I am a scripteed AI system coded by:Vinay sharma\n");
 
- coord.X = x;
-
- coord.Y = y;
-
- SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-
+goto message;
 }
-void GotoXY(int x, int y)
-{
-    HANDLE a;
-    COORD b;
-    fflush(stdout);
-    b.X = x;
-    b.Y = y;
-    a = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(a,b);
- }
-void load(){
-    int row,col,r,c,q;
-    gotoxy(36,14);
-    printf("loading...");
-    gotoxy(30,15);
-    for(r=1;r<=20;r++){
-    for(q=0;q<=100000000;q++);//to display the character slowly
-    printf("%c",177);}
-    getch();
-}
-void Down()
-{
-    int i;
-    for(i=0;i<=(head.y-bend[bend_no].y)&&len<length;i++)
-    {
-        GotoXY(head.x,head.y-i);
-        {
-            if(len==0)
-                printf("v");
-            else
-                printf("*");
-        }
-        body[len].x=head.x;
-        body[len].y=head.y-i;
-        len++;
-    }
-    Bend();
-    if(!kbhit())
-        head.y++;
-}
-void Delay(long double k)
-{
-    Score();
-    long double i;
-    for(i=0;i<=(10000000);i++);
-}
-void ExitGame()
-{
-    int i,check=0;
-    for(i=4;i<length;i++)   //starts with 4 because it needs minimum 4 element to touch its own body
-    {
-        if(body[0].x==body[i].x&&body[0].y==body[i].y)
-        {
-            check++;    //check's value increases as the coordinates of head is equal to any other body coordinate
-        }
-        if(i==length||check!=0)
-            break;
-    }
-    if(head.x<=10||head.x>=70||head.y<=10||head.y>=30||check!=0)
-    {
-        life--; 
-        if(life>=0)
-        {
-            head.x=25;
-            head.y=20;
-            bend_no=0;
-            head.direction=RIGHT;
-            Move();
-        }
-        else
-        {
-            system("cls");
-            printf("All lives completed\nBetter Luck Next Time!!!\nPress any key to quit the game\n");
-            record();
-            exit(0);
-        }
-    }
-}
-void Food()
-{
-    if(head.x==food.x&&head.y==food.y)
-    {
-        length++;
-        time_t a;
-        a=time(0);
-        srand(a);
-        food.x=rand()%70;
-        if(food.x<=10)
-            food.x+=11;
-        food.y=rand()%30;
-        if(food.y<=10)
-
-            food.y+=11;
-    }
-    else if(food.x==0)/*to create food for the first time coz global variable are initialized with 0*/
-    {
-        food.x=rand()%70;
-        if(food.x<=10)
-            food.x+=11;
-        food.y=rand()%30;
-        if(food.y<=10)
-            food.y+=11;
-    }
-}
-void Left()
-{
-    int i;
-    for(i=0;i<=(bend[bend_no].x-head.x)&&len<length;i++)
-    {
-        GotoXY((head.x+i),head.y);
-       {
-                if(len==0)
-                    printf("<");
-                else
-                    printf("*");
-        }
-        body[len].x=head.x+i;
-        body[len].y=head.y;
-        len++;
-    }
-    Bend();
-    if(!kbhit())
-        head.x--;
+else if(strcasecmp("full form of acco",s)==0||strcasecmp("define acco",s)==0||strcasecmp("what is acco",s)==0)
+{ 
+printf("A=Artificial\nC=Computer\nC=Command\nO=Operator\n");
+goto message;
 
 }
-void Right()
+else if(strcasecmp("not fine",s)==0||strcasecmp("fever",s)==0||strcasecmp("exam pressure",s)==0||strcasecmp("cold",s)==0||strcasecmp("not good",s)==0)
 {
-    int i;
-    for(i=0;i<=(head.x-bend[bend_no].x)&&len<length;i++)
-    {
-        //GotoXY((head.x-i),head.y);
-        body[len].x=head.x-i;
-        body[len].y=head.y;
-        GotoXY(body[len].x,body[len].y);
-        {
-            if(len==0)
-                printf(">");
-            else
-                printf("*");
-        }
-        /*body[len].x=head.x-i;
-        body[len].y=head.y;*/
-        len++;
-    }
-    Bend();
-    if(!kbhit())
-        head.x++;
+	printf("Ohhh,:-(\n");
+	printf("Don't worry,i will help you\n");
+	goto message;
+	
 }
-void Bend()
-{
-    int i,j,diff;
-    for(i=bend_no;i>=0&&len<length;i--)
-    {
-            if(bend[i].x==bend[i-1].x)
-            {
-                diff=bend[i].y-bend[i-1].y;
-                if(diff<0)
-                    for(j=1;j<=(-diff);j++)
-                    {
-                        body[len].x=bend[i].x;
-                        body[len].y=bend[i].y+j;
-                        GotoXY(body[len].x,body[len].y);
-                        printf("*");
-                        len++;
-                        if(len==length)
-                            break;
-                    }
-                else if(diff>0)
-                    for(j=1;j<=diff;j++)
-                    {
-                        /*GotoXY(bend[i].x,(bend[i].y-j));
-                        printf("*");*/
-                        body[len].x=bend[i].x;
-                        body[len].y=bend[i].y-j;
-                        GotoXY(body[len].x,body[len].y);
-                        printf("*");
-                        len++;
-                        if(len==length)
-                            break;
-                    }
-            }
-        else if(bend[i].y==bend[i-1].y)
-        {
-            diff=bend[i].x-bend[i-1].x;
-            if(diff<0)
-                for(j=1;j<=(-diff)&&len<length;j++)
-                {
-                    /*GotoXY((bend[i].x+j),bend[i].y);
-                    printf("*");*/
-                    body[len].x=bend[i].x+j;
-                    body[len].y=bend[i].y;
-                    GotoXY(body[len].x,body[len].y);
-                        printf("*");
-                   len++;
-                   if(len==length)
-                           break;
-               }
-           else if(diff>0)
-               for(j=1;j<=diff&&len<length;j++)
-               {
-                   /*GotoXY((bend[i].x-j),bend[i].y);
-                   printf("*");*/
-                   body[len].x=bend[i].x-j;
-                   body[len].y=bend[i].y;
-                   GotoXY(body[len].x,body[len].y);
-                       printf("*");
-                   len++;
-                   if(len==length)
-                       break;
-               }
-       }
-   }
-}
-void Boarder()
-{
-   system("cls");
-   int i;
-   GotoXY(food.x,food.y);   /*displaying food*/
-       printf("F");
-   for(i=10;i<71;i++)
-   {
-       GotoXY(i,10);
-           printf("!");
-       GotoXY(i,30);
-           printf("!");
-   }
-   for(i=10;i<31;i++)
-   {
-       GotoXY(10,i);
-           printf("!");
-       GotoXY(70,i);
-       printf("!");
-   }
-}
-void Print()
-{
-   //GotoXY(10,12);
-   printf("\tWelcome to the mini Snake game.(press any key to continue)\n");
-  getch();
-   system("cls");
-   printf("\tGame instructions:\n");
-   printf("\n-> Use arrow keys to move the snake.\n\n-> You will be provided foods at the several coordinates of the screen which you have to eat. Everytime you eat a food the length of the snake will be increased by 1 element and thus the score.\n\n-> Here you are provided with three lives. Your life will decrease as you hit the wall or snake's body.\n\n-> YOu can pause the game in its middle by pressing any key. To continue the paused game press any other key once again\n\n-> If you want to exit press esc. \n");
-   printf("\n\nPress any key to play game...");
-   if(getch()==27)
-   exit(0);
-}
-void record(){
-   char plname[20],nplname[20],cha,c;
-   int i,j,px;
-   FILE *info;
-   info=fopen("record.txt","a+");
-   getch();
-   system("cls");
-   printf("Enter your name\n");
-   scanf("%[^\n]",plname);
-   //************************
-   for(j=0;plname[j]!='\0';j++){ //to convert the first letter after space to capital
-   nplname[0]=toupper(plname[0]);
-   if(plname[j-1]==' '){
-   nplname[j]=toupper(plname[j]);
-   nplname[j-1]=plname[j-1];}
-   else nplname[j]=plname[j];
-   }
-   nplname[j]='\0';
-   //*****************************
-   //sdfprintf(info,"\t\t\tPlayers List\n");
-   fprintf(info,"Player Name :%s\n",nplname);
-    //for date and time
 
-   time_t mytime;
-  mytime = time(NULL);
-  fprintf(info,"Played Date:%s",ctime(&mytime));
-     //**************************
-     fprintf(info,"Score:%d\n",px=Scoreonly());//call score to display score
-     //fprintf(info,"\nLevel:%d\n",10);//call level to display level
-   for(i=0;i<=50;i++)
-   fprintf(info,"%c",'_');
-   fprintf(info,"\n");
-   fclose(info);
-   printf("wanna see past records press 'y'\n");
-   cha=getch();
-   system("cls");
-   if(cha=='y'){
-   info=fopen("record.txt","r");
-   do{
-       putchar(c=getc(info));
-       }while(c!=EOF);}
-     fclose(info);
-}
-int Score()
+else if(strcasecmp("your name",s)==0)
 {
-   int score;
-   GotoXY(20,8);
-   score=length-5;
-   printf("SCORE : %d",(length-5));
-   score=length-5;
-   GotoXY(50,8);
-   printf("Life : %d",life);
-   return score;
+printf("My name is :ACCO\n");
+
+goto message;
 }
-int Scoreonly()
+else if(strcasecmp("good morning",s)==0||strcasecmp("good evening",s)==0||strcasecmp("good after noon",s)==0||strcasecmp("good night",s)==0)
+{ 
+printf("%s Sir \n",s);
+goto message;
+
+}
+else if(strcasecmp("hi",s)==0){
+
+printf("Hi\nHow are you?\n");
+goto message;
+}
+else if(strcasecmp("fine",s)==0)
+{ printf("Glad to hear it\n");
+goto message;
+}
+else if(strcasecmp("time",s)==0||strcasecmp("show me time",s)==0){
+
+time_t s,val=1;
+	struct tm*current_time;
+	s=time(NULL);
+	current_time = localtime(&s);
+	printf("%02d:%02d:%02d",current_time->tm_hour,current_time->tm_min,current_time->tm_sec);
+goto message;
+}
+else if(strcasecmp("date",s)==0)
 {
-int score=Score();
-system("cls");
-return score;
+	time_t tt;
+	struct tm*ti;
+	time(&tt);
+ti=localtime(&tt);
+ printf("%d/%d/%d",asctime(ti));
+ printf("correct error please");
+ goto message;
+ 
 }
-void Up()
+else if(strcasecmp("add",s)==0)
 {
-   int i;
-   for(i=0;i<=(bend[bend_no].y-head.y)&&len<length;i++)
-   {
-       GotoXY(head.x,head.y+i);
-       {
-           if(len==0)
-               printf("^");
-           else
-               printf("*");
-       }
-       body[len].x=head.x;
-       body[len].y=head.y+i;
-       len++;
-   }
-   Bend();
-   if(!kbhit())
-       head.y--;
+	printf("Enter two value:A&B\n");
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("Addition:%d\n",a+b);
+		goto message;
 }
+else if(strcasecmp("multiply",s)==0)
+{	printf("Enter two value:A&B\n");
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d",a*b);
+	goto message;
+}
+else if(strcasecmp("divide",s)==0)
+	{printf("Enter two value:A&B\n");
+	float a,b;
+	scanf("%f%f",&a,&b);
+	printf("%f",a/b);
+	goto message;
+	}
+else if(strcasecmp("open calculator",s)==0)
+{system("cmd /c calc");
+goto message;
+}
+else if(strcasecmp("open browser",s)==0||strcasecmp("open google",s)==0||strcasecmp("google",s)==0)
+{ system("explorer http://www.google.com");
+goto message;
+}
+else if(strcasecmp("open youtube",s)==0||strcasecmp("youtube",s)==0)
+{
+	system("explorer https://www.youtube.com/");
+	goto message;
+}
+else if(strcasecmp("analyze system",s)==0||strcasecmp("tree",s)==0)
+{
+	system("cmd /c tree");
+	goto message;
+}
+else if(strcasecmp("open wordpad",s)==0||strcasecmp("wordpad",s)==0)
+{
+	system("cmd /c write");
+	goto message;
+}
+else if(strcasecmp("bye",s)==0)
+{ printf("ACCO:-Bye sir ");
+}
+else if(strcasecmp("open paint",s)==0||strcasecmp("paint",s)==0)
+{
+	system("mspaint");
+	goto message;
+}
+else if(strcasecmp("what about you",s)==0){
+	printf("Basically I AM GOOD\n");
+	goto message;
+}
+else if(strcasecmp("who vinay sharma",s)==0||strcasecmp("who is vinay sharma",s)==-0||strcasecmp("vinay sharma",s)==0){
+
+printf("Vinay sharma is my creator and designer \n studies first year of B.Technology\n");
+goto message;}
+else
+{ 
+
+    ShellExecute(NULL, "open",s, NULL, NULL, SW_SHOWNORMAL); 
+    printf("i did'nt get that'\n");
+    goto message;
+}
+}
+
+
 
